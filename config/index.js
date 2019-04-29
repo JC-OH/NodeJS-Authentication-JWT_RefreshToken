eb99d14d89bee85eacd14f503fbe89fa0be9a874
Jicheol Oh <jicheol@gmail.com>
2019-04-29 09:02:47 -0700

    Creating Server and adding routes

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports {
+    secret: "some-secret-shit-goes-here",
+    refreshTokenSecret: "some-secret-refresh-token-shit",
+    port: 3000,
+    tokenLife: 900,
+    refreshTokenLife: 86400
+}
